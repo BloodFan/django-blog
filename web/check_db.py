9768bc2329b183048dcadf06bcc6049e1e4282fb
Сергей Кимайкин <017Alykar1990@mail.ru>
2024-06-08 19:44:47 +1000

    Check DB Engine_2

--- web/check_db.py
+++ web/check_db.py
@@ -1,5 +1,9 @@
+import os
+import django
 from django.conf import settings
 
 if __name__ == '__main__':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.settings_prod')
     engine = settings.DATABASES['default']['ENGINE']
+    django.setup()
     print(engine)
