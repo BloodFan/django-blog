99b95d6274946d2bcdd8d4783bf8f427f91093c7
Сергей Кимайкин <017Alykar1990@mail.ru>
2024-06-10 22:04:57 +1000

    smtp_v1

--- web/src/additional_settings/smtp_settings.py
+++ web/src/additional_settings/smtp_settings.py
@@ -4,11 +4,18 @@ EMAIL_HOST = environ.get('EMAIL_HOST')
 EMAIL_PORT = int(environ.get('EMAIL_PORT', 465))
 EMAIL_HOST_USER = environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = environ.get('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = environ.get('DEFAULT_FROM_EMAIL')
+# DEFAULT_FROM_EMAIL = environ.get('DEFAULT_FROM_EMAIL')
 EMAIL_TIMEOUT = int(environ.get('EMAIL_TIMEOUT', 15))
 EMAIL_USE_SSL = int(environ.get('EMAIL_USE_SSL', 0))
-EMAIL_USE_TLS = int(environ.get('EMAIL_USE_TLS', 0))
+# EMAIL_USE_TLS = int(environ.get('EMAIL_USE_TLS', 0))
+# SERVER_EMAIL = environ.get('SERVER_EMAIL')
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 SERVER_EMAIL = EMAIL_HOST_USER
+EMAIL_ADMIN = EMAIL_HOST_USER
+
 
 # Available choice: console, smtp, locmem, etc..
-EMAIL_BACKEND = 'django.core.mail.backends.{0}.EmailBackend'.format(environ.get('EMAIL_BACKEND', 'console'))
+# EMAIL_BACKEND = 'django.core.mail.backends.{0}.EmailBackend'.format(environ.get('EMAIL_BACKEND', 'console'))
+
+EMAIL_BACKEND = f'django.core.mail.backends.{environ.get("EMAIL_BACKEND", "console")}.EmailBackend'
